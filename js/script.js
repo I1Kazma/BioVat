c885651e688a2c85742750c32a25638ae740e2d8
Ivan Kazmenko <117230564+I1Kazma@users.noreply.github.com>
2023-08-29 15:32:59 +0200

    added adaptive (992px)

--- js/script.js
+++ js/script.js
@@ -16,3 +16,29 @@ arrowLinks.forEach((svg) => {
     line_2.setAttribute("x1", "66.1611");
   });
 });
+
+let menu = document.querySelector("#menu-top-icon");
+let catalog_button = document.querySelector("#menu-bottom-icon");
+let catalog = document.querySelector(".header__nav-items");
+let header_menu = document.querySelector(".header_menu");
+let menu_close = document.querySelector("#menu-top-close-icon");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  header_menu.classList.toggle("open");
+};
+
+catalog_button.onclick = () => {
+  catalog.classList.toggle("open");
+};
+
+menu_close.onclick = () => {
+  menu.classList.remove("bx-x");
+  header_menu.classList.remove("open");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  header_menu.classList.remove("open");
+  catalog.classList.remove("bx-x");
+};
